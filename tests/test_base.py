a3f4300ece479e620983a55276c2acb4a75bc577
Gabriele Modena <gm@nowave.it>
2020-01-12 01:39:59 +0100

    WIP: add sklearn classifier test

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,7 @@
 import pytest
 import tensorflow as tf
-from tensorfm.base import FactorizationMachine
+from tensorfm.base import BaseFactorizationMachine
+from tensorfm.sklearn import FactorizationMachineClassifier
 
 import numpy as np
 
@@ -27,9 +28,27 @@ def test_base_fit():
             .batch(200)
             .shuffle(1000)
     )
+    optimizer = tf.keras.optimizers.Adagrad(learning_rate=tf.constant(0.01))
 
-    model = FactorizationMachine(train_dataset)
-    a = model.fit()
+    model = BaseFactorizationMachine(train_dataset, optimizer=optimizer)
+    clf = model.fit()
+    assert clf is not None
 
+    # predict multiple instances
     r = model.predict(tf.cast(x_data, tf.float32))
+    assert r.shape == [x_data.shape[0], 1]
 
+    # predict single instance
+    # pred = model.predict(tf.cast(x_data[0], tf.float32))
+    # assert pred.shape == [1, 1]
+
+def test_sklearn_classifier():
+    train_dataset = (
+        tf.data.Dataset.from_tensor_slices((tf.reshape(x_data, [-1, x_data.shape[1]]), y_data))
+            .batch(200)
+            .shuffle(1000)
+    )
+
+    clf = FactorizationMachineClassifier()
+
+    assert clf is not None
